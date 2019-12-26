e74fbfc08ab90aed865073fd6e9ff0950ed380ae
Сергей Азаренко <s_azarenko@outlook.com>
2019-12-26 21:14:41 +0300

    Перед введением автоопределения типа

--- OOP_kursov/Source.cpp
+++ OOP_kursov/Source.cpp
@@ -1,25 +1,55 @@
 #include "List.h"
 #include <iostream>
 
+#define TYPE_0 int
+#define TYPE_1 double
 
+bool add_column(List<BaseObject>* list, int id) {
+	if (id == 0) {
+		list->append(new List<BaseObject>(new List<TYPE_0>(typeid(TYPE_0).hash_code()))); // Создание столбика int-ов
+	}
+	else if (id == 1) {
+		list->append(new List<BaseObject>(new List<TYPE_1>(typeid(TYPE_1).hash_code()))); // Создание столбика double-ов
+	}
+	else {
+		std::cout << "The type of created column is not defined!" << "\n";
+		return false;
+	}
+	return true;
+}
 
-double get_cell(List<BaseObject>* test2_list, int col, int row) {
+double get_cell(List<BaseObject>* list, int col, int row) {
 	auto type = typeid(int).hash_code(); // Определяется из типа создаваемой колонки
 
 	double result;
+	bool was_result_get = false; // Был ли получен результат?
 
-	if (type == typeid(int).hash_code()) {
-		auto a = (((List<int>*)test2_list->get_elem(col)->get_obj())->get_elem(row)->get_obj());
+	auto always_get = list->get_elem(col)->get_obj(); // Всегда пытаемся найти нужный столбик. Если его нет - получим nullptr
+
+	if (type == typeid(TYPE_0).hash_code()) {
+		auto a = (((List<TYPE_0>*)list->get_elem(col)->get_obj())->get_elem(row)->get_obj());
 		if (a) {
-			int _result = *a;
+			TYPE_0 _result = *a;
 			result = (double)_result;
+			was_result_get = true;
 		}
 
 	}
-	else if (type == typeid(double).hash_code()) {
-		double _result = *(((List<double>*)test2_list->get_elem(col)->get_obj())->get_elem(row)->get_obj());
-		result = (double)_result;
+	else if (type == typeid(TYPE_1).hash_code()) {
+		auto a = (((List<TYPE_1>*)list->get_elem(col)->get_obj())->get_elem(row)->get_obj());
+		if (a) {
+			TYPE_1 _result = *a;
+			result = (double)_result;
+			was_result_get = true;
+		}
 	}
+	else {
+		std::cout << "The type of cell [" << col << ";" << row << "] is not defined!" << "\n";
+		return 0.0;
+	}
+
+	if (!was_result_get)
+		std::cout << "The cell[" << col << ";" << row << "] is empty!" << "\n";
 
 	return &result?result:0.0;
 }
@@ -29,9 +59,7 @@ int main() {
 
 	List<BaseObject> test2_list;
 
-	test2_list.append(new List<BaseObject>(new List<int>(typeid(int).hash_code()))); // Создание столбика "int"-ов
-
-	
+	add_column(&test2_list, 0);
 
 	auto added_int = new int(5);
 
